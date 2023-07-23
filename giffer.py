14fb6e8f40db899ef3c602695a3bb2356b0056c4
Isacco Faglioni <isaccofaglioni2000@gmail.com>
2023-07-23 16:40:49 +0200

    main and giffer labor limae

--- giffer.py
+++ giffer.py
@@ -10,14 +10,16 @@ def gif_maker(path, gif_name):
     path: string, it is the path to the data to make the gif
     gif_name: name of the gif file, it saves it in current dir
     """
-    filenames_ = os.listdir(path1)
+    filenames_ = os.listdir(path)
     filenames = np.sort(filenames_)
     img = []
     for fn in filenames:    
-        img.append(imageio.imread(path1 + fn))
-        imageio.mimsave(gif_name, img, duration = 10)
+        img.append(imageio.imread(path + fn))
+    for fn_ in filenames[::-1]:
+        img.append(imageio.imread(path + fn_))
+    imageio.mimsave(gif_name, img, duration = len(filenames)/10)
 
-gif_maker(path1, "julia_set.gif")
-
+#gif_maker(path1, "julia_set.gif")
+gif_maker(path2, "mandelbrot_set.gif")
 
 
--- main.cpp
+++ main.cpp
@@ -5,13 +5,13 @@ int main() {
   std::complex<double> c (0.3, -0.45);
     std::string img = "README_IMG";
   j.julia_gen(c);
-  j.save_to_file_j(c, img);
+  j.save_to_file_j(c, img); //single image of julia set
   
 
   fractals m(800);
   double scaling_factor = 0.9;
   m.mandel_gen(scaling_factor, -2, -1.13);
-  m.save_to_file_j(scaling_factor, img);
+  m.save_to_file_j(scaling_factor, img); //single image of mandelbrot set
   
 
 
@@ -19,12 +19,12 @@ int main() {
   int num_points = 50;
   double step = 0.01;
   std::string dir_j = "julia_set";
-  generate_julia_set(dim, num_points, step, dir_j);
+  generate_julia_set(dim, num_points, step, dir_j); //images for creating a gif of the julia set
 
-  double ssf = 1.5;
-  double esf = 0.05;
-  double st = 0.1;
-  std::string dir_m = "pd";
-  generate_scaled_outputs(dim, ssf, esf, st, dir_m);
+  double start_scaling_factor = 1.5;
+  double end_scaling_factor= 0.5;
+  double step_ = 0.05;
+  std::string dir_m = "mandelbrot_zoom";
+  generate_scaled_outputs(dim, start_scaling_factor, end_scaling_factor, step_, dir_m);
   
 }
