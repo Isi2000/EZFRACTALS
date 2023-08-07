0c06ad8441d677961c52bc40ff73f7202f75b0c7
Isacco Faglioni <isaccofaglioni2000@gmail.com>
2023-08-07 15:28:13 +0200

    Added main.cpp to use the new fractals.h
    Basically works the same as before but with new functions

--- main.cpp
+++ main.cpp
@@ -1,30 +1,18 @@
 #include "fractals.h"
 
 int main() {
-  fractals j(800);
-  std::complex<double> c (0.3, -0.45);
-    std::string img = "README_IMG";
-  j.julia_gen(c);
-  j.save_to_file_j(c, img); //single image of julia set
-  
+    int dim = 400;
+    double end_scaling_factor = 0.1;
+    double scaling_factor_step = 0.1;
+    const double center_real = -0.74001;
+    const double center_im = 0.0;
 
-  fractals m(800);
-  double scaling_factor = 0.9;
-  m.mandel_gen(scaling_factor, -2, -1.13);
-  m.save_to_file_j(scaling_factor, img); //single image of mandelbrot set
-  
+    Mandelbrot test_M_m_i(dim);
+    test_M_m_i.mandelbrot_multiple_images(end_scaling_factor, scaling_factor_step, center_real, center_im);
 
+    int num_points = 5;
+    double step = 0.1;
+    Julia test_J_m_i(dim);
+    test_J_m_i.julia_multiple_images(num_points, step);
 
-  int dim = 400;
-  int num_points = 50;
-  double step = 0.01;
-  std::string dir_j = "julia_set";
-  generate_julia_set(dim, num_points, step, dir_j); //images for creating a gif of the julia set
-
-  double start_scaling_factor = 1.5;
-  double end_scaling_factor= 0.5;
-  double step_ = 0.05;
-  std::string dir_m = "mandelbrot_zoom";
-  generate_scaled_outputs(dim, start_scaling_factor, end_scaling_factor, step_, dir_m);
-  
 }
